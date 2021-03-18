975480ba7b6a6f2f9c8060eea5fa2ad6b18f6e18
hwjfordev <hwjfordev@gmail.com>
2021-03-18 22:24:36 +0800

    export MoPubBanner

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import AdLibSDK from "./src/AdLibSDK";
 import RNMoPubInterstitial from "./src/RNMoPubInterstitial";
 import RNMoPubRewardedVideo from "./src/RNMoPubRewardedVideo";
 import { INativeAdConfig, RNNativeAdView } from "./src/RNNativeAdView";
+import MoPubBanner from './src/MoPubBanner'
 
 module.exports = {
     AdLibSDK,
     INativeAdConfig,
     RNMoPubInterstitial,
     RNMoPubRewardedVideo,
-    RNNativeAdView
+    RNNativeAdView,
+    MoPubBanner
 };
